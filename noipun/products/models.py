5c9d59adb7d0a717bd98a56ea3c5c01e86a9ce69
Maharaf Hossen <maharafhossen103@gmail.com>
2023-12-16 12:25:49 +0600

    migrate succesfully

--- noipun/products/models.py
+++ noipun/products/models.py
@@ -1,40 +1,30 @@
 from django.db import models
+from core.models import CustomUser
+
 
 # Create your models here.
 
-
-class Product(models.Model):
-    # product id will Auto genatreted
-    product_title = models.CharField( max_length=100,null=False,blank=False,verbose_name="Product_Title")
-    # images will be inherited
-    description = models.TextField(null=False,blank=False,verbose_name="Product_Description")
-    # offers will be inherited
-    price = models.FloatField(default=0,null=False,blank=False,verbose_name="Price")
-    # seller wil be inherited
-    # tags wil be inherited
-    ratting =models.FloatField(default=0,verbose_name="Ratting")
-    # review will be inherited
-    is_available = models.BooleanField(default=True,verbose_name="Available_Status")
-    total_buyed =  models.IntegerField(default = 0,verbose_name="Total_Buyed")
-    delivery_is_free = models.BooleanField(default=False,verbose_name="Delivery_Status")
-    weight =  models.FloatField(default = 0,verbose_name="Weight")
-    # slug i cant understand
-    created_at = models.DateTimeField( auto_now=False, auto_now_add=True,editable=False,verbose_name="Created_At")
-    modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='Last_Modified')
-
-
-
 class Categeory(models.Model):
-    # Categeory Id wil be Auto generated
+    categeory_id = models.AutoField(primary_key=True)
     categeory_name = models.CharField(max_length=50,null=False,blank=False,verbose_name="Categeory_Name")
     categeory_description = models.TextField(verbose_name="Categeory_Description", null = True,blank=True)
     created_at = models.DateTimeField( auto_now=False, auto_now_add=True,editable=False,verbose_name="Created_At")
     modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='Last_Modified')
 
 
-class Discount(models.Model):
+
+class Images(models.Model):
+    image_id = models.AutoField(primary_key=True)
+    # seller_id =
+
+    image = models.URLField(verbose_name="image")
+    created_at = models.DateTimeField( auto_now=False, auto_now_add=True,editable=False,verbose_name="Created_At")
+    modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='Last_Modified')
+
+class Offers(models.Model):
     # Discount id will be auto generated 
-    discount_name =  models.CharField( max_length=100,nul=False,blank=False,verbose_name="Discount_Name")   
+
+    discount_name =  models.CharField( max_length=100,null=False,blank=False,verbose_name="Discount_Name")   
     discount_description = models.TextField(verbose_name="Discount_Description", null = True,blank=True)
     # categeory discount 
     discount_percent = models.IntegerField(null=False,blank=False,verbose_name="Discount") 
@@ -44,20 +34,38 @@ class Discount(models.Model):
     modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='Last_Modified')
     active =  models.BooleanField(default=False)
 
+class Product(models.Model):
+    product_id  = models.AutoField(primary_key=True)
+    product_title = models.CharField( max_length=100,null=False,blank=False,verbose_name="Product_Title")
+    image = models.ForeignKey(Images, verbose_name="images", on_delete=models.CASCADE)
+    description = models.TextField(null=False,blank=False,verbose_name="Product_Description")
+    offers = models.ManyToManyField(Offers, verbose_name="offers")
+    # offers = models.ManyToManyField("app.Model", verbose_name=_(""))
+    price = models.FloatField(default=0,null=False,blank=False,verbose_name="Price")
+    seller_id = models.ForeignKey(CustomUser, verbose_name="sellerId", on_delete=models.CASCADE)
+    tags = models.ForeignKey(Categeory, verbose_name="categeory", on_delete=models.CASCADE)
+    ratting =models.FloatField(default=0,verbose_name="Ratting")
+    reviews= models.ForeignKey("Reviews", verbose_name="reviews", on_delete=models.CASCADE,related_name="product_review")
+    is_available = models.BooleanField(default=True,verbose_name="Available_Status")
+    total_buyed =  models.IntegerField(default = 0,verbose_name="Total_Buyed")
+    delivery_is_free = models.BooleanField(default=False,verbose_name="Delivery_Status")
+    weight =  models.FloatField(default = 0,verbose_name="Weight")
+    # slug i cant understand
+    created_at = models.DateTimeField( auto_now=False, auto_now_add=True,editable=False,verbose_name="Created_At")
+    modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='Last_Modified')
+
 class Reviews(models.Model):
-    # Review  id will auto genrated
-    # user =
-    product_id = models.ForeignKey("Product",  on_delete=models.CASCADE)
+    review_id =  models.AutoField(primary_key=True)
+    user = models.ForeignKey(CustomUser, verbose_name="user", on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE,related_name = "review_product")
     review =  models.TextField(null=False,blank=False)
     created_at = models.DateTimeField( auto_now=False, auto_now_add=True,editable=False,verbose_name="Created_At")
     modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='Last_Modified')
 
-class Images(models.Model):
-    # Image id 
-    # seller_id =
-    image = models.ImageField( upload_to=None,)
-    created_at = models.DateTimeField( auto_now=False, auto_now_add=True,editable=False,verbose_name="Created_At")
-    modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='Last_Modified')
+
+
+
+
 
 
 
