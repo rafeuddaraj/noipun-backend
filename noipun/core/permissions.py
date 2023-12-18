84953f3f9e3208d661da2af9fbe8cb96c5bd6070
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2023-12-18 11:43:12 +0600

    Seller permission added

--- noipun/core/permissions.py
+++ noipun/core/permissions.py
@@ -13,6 +13,17 @@ class Private(BasePermission):
         return bool(request.user and request.user.is_staff and request.user.is_superuser)
 
 
+class SellerOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+            return True
+
+        return (
+            request.user.is_authenticated
+            and str(request.user.id) == view.kwargs.get('pk', '')
+        )
+
+
 class ReviewOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
