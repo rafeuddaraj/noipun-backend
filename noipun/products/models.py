4e8a318ac648cb9d9cb98bab6a161e58b8a43cb4
Maharaf Hossen <maharafhossen103@gmail.com>
2023-12-24 23:17:44 +0600

    updated api

--- noipun/products/models.py
+++ noipun/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from core.models import CustomUser
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,8 +8,8 @@ from core.models import CustomUser
 class Category(models.Model):
     category_name = models.CharField(max_length=50,null=True,blank=True,verbose_name="categoryName")
     category_description = models.TextField(verbose_name="categoryDescription", null = True,blank=True)
-    created_at = models.DateTimeField( auto_now=False, auto_now_add=True,editable=False,verbose_name="createdAt")
-    modified = models.DateTimeField(auto_now=False,auto_now_add=True, editable=False,verbose_name='lastModified')
+    created_at = models.DateTimeField(  auto_now_add=True,editable=False,verbose_name="createdAt")
+    modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='lastModified')
     user = models.ManyToManyField(to=CustomUser,related_name='category',blank=True,verbose_name = "user")
     def __str__(self):
         return self.category_name
@@ -20,46 +21,46 @@ class Category(models.Model):
 
 class Images(models.Model):
     image_id = models.AutoField(primary_key=True)
-    # seller_id =
-
     image = models.URLField(verbose_name="image",max_length=1000,null=True,blank=True)
-    created_at = models.DateTimeField( auto_now=False, auto_now_add=True,editable=False,verbose_name="createdAt")
-    modified = models.DateTimeField(auto_now=False, auto_now_add=True, editable=False,verbose_name='lastModified')
+    product = models.ForeignKey("Product", verbose_name="Product", on_delete=models.CASCADE,related_name="image")
+    created_at = models.DateTimeField( auto_now_add=True,editable=False,verbose_name="createdAt")
+    modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='lastModified')
     class Meta:
         verbose_name_plural = 'Images'
+
 
 class Offers(models.Model):
     discount_id =   models.AutoField(primary_key=True,verbose_name="discountId")
     discount_name =  models.CharField( max_length=100,null=True,blank=True,verbose_name="discountName")   
     discount_description = models.TextField(verbose_name="discountDescription", null = True,blank=True)
     # Category discount 
+    product = models.ForeignKey("Product", verbose_name="product",on_delete=models.CASCADE)
     discount_percent = models.IntegerField(null=True,blank=True,verbose_name="discount") 
     discount_start = models.DateTimeField( auto_now=False, auto_now_add=False,editable=True,verbose_name="discountStart")
     discount_end = models.DateTimeField( auto_now=False, auto_now_add=False,editable=True,verbose_name="discountEnd")
-    created_at = models.DateTimeField( auto_now=False, auto_now_add=True,editable=False,verbose_name="createdAt")
-    modified = models.DateTimeField(auto_now=False, editable=False,verbose_name='lastModified')
+    created_at = models.DateTimeField( auto_now_add=True,editable=False,verbose_name="createdAt")
+    modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='lastModified')
     active =  models.BooleanField(default=False)
+
     class Meta:
         verbose_name_plural = 'Offers'
 
 class Product(models.Model):
     product_id  = models.AutoField(primary_key=True,verbose_name="productName")
     product_title = models.CharField( max_length=100,null=True,blank=True,verbose_name="productTitle")
-    image = models.ForeignKey(Images, verbose_name="images", on_delete=models.CASCADE)
     description = models.TextField(null=True,blank=True,verbose_name="productDescription")
-    offers = models.ForeignKey(Offers, verbose_name="offers",on_delete=models.CASCADE)
-    price = models.FloatField(default=0,null=True,blank=True,verbose_name="price")
+    price = models.FloatField(default=0,null=True,blank=True,verbose_name="price",)
     seller_id = models.ForeignKey(CustomUser, verbose_name="sellerId", on_delete=models.CASCADE)
-    tags = models.ForeignKey(Category, verbose_name="product", on_delete=models.CASCADE)
+    tags = models.ForeignKey(Category, verbose_name="Category", on_delete=models.CASCADE)
     ratting =models.FloatField(default=0,verbose_name="ratting")
-    reviews= models.ForeignKey("Reviews", verbose_name="reviews", on_delete=models.CASCADE,related_name="product_review")
     is_available = models.BooleanField(default=True,verbose_name="availableStatus",null=True,blank=True)
     total_buyed =  models.IntegerField(default = 0,verbose_name="totalBuyed")
     delivery_is_free = models.BooleanField(default=False,verbose_name="deliveryStatus")
     weight =  models.FloatField(default = 0,verbose_name="weight",null=True,blank=True)
     slug=models.SlugField(verbose_name = "slugField",max_length = 100,default=None)
-    created_at = models.DateTimeField( auto_now=False, auto_now_add=True,editable=False,verbose_name="CreatedAt")
-    modified = models.DateTimeField(auto_now=False, editable=False,verbose_name='lastModified')
+    created_at = models.DateTimeField(  auto_now_add=True,editable=False,verbose_name="CreatedAt")
+    modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='lastModified')
+
     class Meta:
         verbose_name_plural = 'Products'
 
@@ -68,17 +69,9 @@ class Reviews(models.Model):
     user = models.ForeignKey(CustomUser, verbose_name="user", on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE,related_name = "review_product")
     review =  models.TextField(null=True,blank=True)
-    created_at = models.DateTimeField( auto_now=False, auto_now_add=True,editable=False,verbose_name="createdAt")
-    modified = models.DateTimeField(auto_now=False, editable=False,verbose_name='lastModified')
+    created_at = models.DateTimeField( auto_now_add=True,editable=False,verbose_name="createdAt")
+    modified = models.DateTimeField(auto_now=True, editable=False,verbose_name='lastModified')
     class Meta:
         verbose_name_plural = 'Reviews'
-
-
-
-
-
-
-
-
 
 
