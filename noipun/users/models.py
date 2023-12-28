c7955ba2d798a902cd9326d0ca7eaee005c8a47d
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2023-12-28 22:17:54 +0600

    address update

--- noipun/users/models.py
+++ noipun/users/models.py
@@ -4,14 +4,17 @@ from core.models import CustomUser
 
 
 class NID(models.Model):
-    nid_id = models.AutoField(verbose_name='nidId',primary_key=True)
-    seller = models.ForeignKey(to=CustomUser,on_delete=models.CASCADE,related_name='nid_seller')
+    nid_id = models.AutoField(verbose_name='nidId', primary_key=True)
+    seller = models.ForeignKey(
+        to=CustomUser, on_delete=models.CASCADE, related_name='nid_seller')
+    address = models.TextField(verbose_name='address', null=True, blank=True)
     front = models.URLField(verbose_name='front')
     back = models.URLField(verbose_name='back')
-    created_at = models.DateTimeField(auto_now_add=True,verbose_name='createdAt')
-    
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='createdAt')
+
     def __str__(self) -> str:
-         return self.seller.email
-    
+        return self.seller.email
+
     class Meta:
-         verbose_name_plural = 'National ID Cards'
+        verbose_name_plural = 'National ID Cards'
